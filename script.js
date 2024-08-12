de2a3c1609ba08f256ccecfdcf862ccf3d8c0373
AvinashMSangle <avinashMS.3119@proton.me>
2024-08-12 06:31:52 +0000

    second part

--- script.js
+++ script.js
@@ -14,14 +14,87 @@
 //        console.log("Mast Ahe"+name);
 //    }
 //    kaskay(" Avi");
-function greeting(greetWord,name){
-    if(greetWord === undefined)greetWord = "Hello";
-    if(name === undefined)name = "User";
-           console.log(greetWord,name);
-       }
-       greeting("kaskay","Avi");
-       greeting();
-       greeting("Good Morning","Sir");
+
+
+// function greeting(greetWord,name){
+//     if(greetWord === undefined)greetWord = "Hello";
+//     if(name === undefined)name = "User";
+//            console.log(greetWord,name);
+//        }
+//        greeting("kaskay","Avi");
+//        greeting();
+//        greeting("Good Morning","Sir");
+
+
+
+       /*
+function weekDay(day){
+    day = Number(day);
+    if(day==1){
+        console.log("monday");
+    }else if(day==2){
+        console.log("tuesday");
+    }else if(day==3){
+        console.log("wednesday");
+    }
+    else if(day==4){
+        console.log("thursday");
+    }else if(day==5){
+        console.log("friday");
+    }else if(day==6){
+        console.log("saturday");
+    }else if(day==7){
+        console.log("sunday");
+    }else {
+        console.log("invalid day");}
+}
+weekDay(9);
+*/
+/*function weekDay(day){
+    day= Number(day);
+    switch(day){
+        case 1:
+            console.log('mon');
+            
+    }
+}
+*/
+
+
+
+// const sports = ["cricket","kabbadi","hocky","football"];
+// sports[2]
+// console.log(sports.length);
+
+
+
+// function weekDay(dayInput){
+//     dayInput=Number(dayInput);
+
+// const days =["mon","tue","wed","thurs",
+//            "fri","sat","sun"];
+
+// const day = days[dayInput-1];
+
+// return day ?? "invalid";
+// }
+// weekDay("9")
+
+
+
+// function sum(num1, num2){
+//     return num1 ** num2;
+// }
+// const x =sum(2,3)
+// console.log(x);
+
+
+// function greet (greetWord = "Hello", name="User"){
+//     // const str = greetWord + " " +name "nice to meet you.";
+//      const str2 = `${greetWord} ${name}, nice to meet you!`;
+
+//      console.log(str2);
+// }
 
        
 
