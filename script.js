739d8bcd42fa8bf48e38a01a92a985dfe03e46ca
AvinashMSangle <avinashMS.3119@proton.me>
2024-08-12 02:45:44 +0000

    first part

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+// const userInput ="Brenden Eich ";
+// if(userInput.trim().includes(" ")){
+//         console.log("it's full name");
+//     }else{
+//         console.log("it's first name");
+//     }
+
+// function kaskay(){
+//     console.log("mast");
+// }kaskay();
+// console.log(kaskay());
+// function kaskay(name){
+//        console.log("Mast Ahe"+name);
+//    }
+//    kaskay(" Avi");
+function greeting(greetWord,name){
+    if(greetWord === undefined)greetWord = "Hello";
+    if(name === undefined)name = "User";
+           console.log(greetWord,name);
+       }
+       greeting("kaskay","Avi");
+       greeting();
+       greeting("Good Morning","Sir");
+
+       
+
