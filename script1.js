d4cf288a9970d2465d3489d9d58b1557e7da97a0
AvinashMSangle <avinashMS.3119@proton.me>
2024-09-16 08:00:27 +0000

    learning js

---
+++ script1.js
@@ -0,0 +1,14 @@
+//Arrays
+const fruits = "apple-banana-orange-mango-kiwi-peach".split('-');
+//push, pop,shift,unshift
+//remove-> returns the remove elememnts
+// add -> returns the new length
+
+// slice: extract new arry
+// const newFruits
+
+
+
+
+
+
